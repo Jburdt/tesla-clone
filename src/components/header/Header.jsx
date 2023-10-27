4ad30547c29322e6904d55d09479030d6cdf86c8
Josh Burd <Jburdt@gmail.com>
2023-10-27 17:19:46 -0400

    created menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,48 @@
 import { useNavigate } from "react-router";
-import './Header.css';
+import "./Header.css";
+import Sidebar from "../sidebar/Sidebar";
+import { useState } from "react";
+import Backdrop from "../backdrop/Backdrop";
 
-const headerElements = [
-  { label: "TESLA", route: "", style: { marginRight: 'auto' }},
+export const navElements = [
+  { label: "TESLA", route: "", style: { marginRight: "auto" } },
   { label: "Model S", route: "model_s" },
   { label: "Model X", route: "model_x" },
   { label: "CyberTruck", route: "cybertruck" },
-  { label: "Menu", route: null, style: { marginLeft: 'auto' }},
+  { label: "Menu", route: null, style: { marginLeft: "auto" } },
 ];
 
 export default function Header() {
   const navigate = useNavigate();
+  const [showSidebar, setShowSidebar] = useState(false);
+
   return (
-    <nav>
-      <ul className="headerList">
-        {headerElements.map(({ label, route, style= {} }) => (
-          <li
-            style={style}
-            key={label}
-            onClick={() => {
-              if (!(label === "Menu")) {
-                navigate(`/${route}`);
-              }
-            }}
-          >
-            {label}
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+      {showSidebar && (
+        <>
+          <Backdrop onClick={() => setShowSidebar(false)} />
+          <Sidebar setShowSidebar={setShowSidebar} />
+        </>
+      )}
+      <nav>
+        <ul className="headerList">
+          {navElements.map(({ label, route, style = {} }) => (
+            <li
+              style={style}
+              key={label}
+              onClick={() => {
+                if (label === "Menu") {
+                  setShowSidebar(true);
+                } else {
+                  navigate(`/${route}`);
+                }
+              }}
+            >
+              {label}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
   );
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { navElements } from "../header/Header";
+import { useNavigate } from "react-router";
+
+const Sidebar = ({ setShowSidebar }) => {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="sidebar">
+      <button onClick={() => setShowSidebar(false)}>X</button>
+      <ul>
+        {navElements.map(({ label, route }) => {
+          if (label === "Menu") return null;
+          return (
+            <li
+              onClick={() => {
+                navigate(`/${route}`);
+              }}
+              key={label}
+            >
+              {label}
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Sidebar;
