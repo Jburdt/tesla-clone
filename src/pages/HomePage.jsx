1098e2ad427b01b962811a1247ef894a3bffedad
Josh Burd <Jburdt@gmail.com>
2023-11-12 21:19:02 -0500

    added model s section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,16 @@
 import HomeVideo from "../components/homeVideo/HomeVideo";
+import ImageBackgroundSection from "../components/imageBackgroundSection/ImageBackgroundSection";
+import modelS from "../assets/modelS.jpg";
 
 const HomePage = () => {
   return (
     <>
       <HomeVideo />
+      <ImageBackgroundSection
+        header="Model S"
+        description="View inventory"
+        backgroundImage={modelS}
+      />
     </>
   );
 };
