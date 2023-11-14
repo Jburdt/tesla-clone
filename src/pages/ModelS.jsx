01e92fcda47c64fb73626fc1a3eaeba3ce438353
Josh Burd <Jburdt@gmail.com>
2023-11-14 14:33:20 -0500

    added bottom section to all landing pages

--- src/pages/ModelS.jsx
+++ src/pages/ModelS.jsx
@@ -1,7 +1,10 @@
 import ImageBackgroundSection from "../components/imageBackgroundSection/ImageBackgroundSection";
 import modelS from "../assets/modelS.jpg";
+import interior from "../assets/teslaInside.jpg";
+import teslaOutside from "../assets/teslaOutside.jpg";
 import modelSMatte from "../assets/modelSMatte.jpg";
 import ImageBackgroundNoText from "../components/ImageBackgroundNoText/ImageBackgroundNoText";
+import ImageGrid from "../components/ImageGrid/ImageGrid";
 
 const featureItems = [
   { title: "3.1 s", desc: "0-60 mph" },
@@ -22,6 +25,14 @@ const ModelS = () => {
       </section>
       <section className="section">
         <ImageBackgroundNoText backgroundImage={modelSMatte} />
+      </section>
+      <section className="section">
+        <ImageGrid
+          image1={teslaOutside}
+          image2={interior}
+          text1="Stay Connected with multi-device Bluetooth, or fast charge devices with wireless and 36-watt USB-C charging."
+          text2="Sublime Sound - A 22-speaker, 960-watt audio system with Active Road Noise Reduction offers the best listening experience wherever you are."
+        />
       </section>
     </div>
   );
--- src/pages/Modelx.jsx
+++ src/pages/Modelx.jsx
@@ -1,7 +1,10 @@
 import ImageBackgroundSection from "../components/imageBackgroundSection/ImageBackgroundSection";
 import modelx from "../assets/model-x.jpg";
 import modelxx from "../assets/modelxx.jpg";
+import teslaInt from "../assets/teslaInside.jpg";
+import teslaOut from "../assets/teslaOutside.jpg";
 import ImageBackgroundNoText from "../components/ImageBackgroundNoText/ImageBackgroundNoText";
+import ImageGrid from "../components/ImageGrid/ImageGrid";
 
 const featureItems = [
   { title: "3.8 s", desc: "0-60 mph" },
@@ -22,6 +25,14 @@ const Modelx = () => {
       </section>
       <section className="section">
         <ImageBackgroundNoText backgroundImage={modelxx} />
+      </section>
+      <section className="section">
+        <ImageGrid
+          image1={teslaInt}
+          image2={teslaOut}
+          text1="Stay Connected with multi-device Bluetooth, or fast charge devices with wireless and 36-watt USB-C charging."
+          text2="Sublime Sound - A 22-speaker, 960-watt audio system with Active Road Noise Reduction offers the best listening experience wherever you are."
+        />
       </section>
     </div>
   );
