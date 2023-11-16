cfb8157507c99080e921f173b0b1c416ccab4782
Josh Burd <Jburdt@gmail.com>
2023-11-16 12:02:33 -0500

    added more styling to each product

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -4,6 +4,7 @@ import ShopProducts from "../components/shopProducts/shopProducts";
 const Shop = () => {
   return (
     <div className="container shop-page">
+      <h1>Products</h1>
       <ShopProducts />
     </div>
   );
