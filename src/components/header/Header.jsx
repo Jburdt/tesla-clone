dd0023626e411081d791d72879106359917baa99
Josh Burd <Jburdt@gmail.com>
2023-11-16 20:42:07 -0500

    started cart

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import "./Header.css";
 
 export const navElements = [
@@ -7,7 +8,8 @@ export const navElements = [
   { label: "Model X", route: "model_x" },
   { label: "Roadster", route: "roadster" },
   { label: "Shop", route: "shop" },
-  { label: "Menu", route: null, style: { marginLeft: "auto" } },
+  { label: <ShoppingCartIcon />, route: "cart", style: { marginLeft: "auto" } },
+  { label: "Menu", route: null },
 ];
 
 export default function Header({ setShowSidebar }) {
@@ -17,21 +19,36 @@ export default function Header({ setShowSidebar }) {
     <>
       <nav>
         <ul className="headerList">
-          {navElements.map(({ label, route, style = {} }) => (
-            <li
-              style={style}
-              key={label}
-              onClick={() => {
-                if (label === "Menu") {
-                  setShowSidebar(true);
-                } else {
-                  navigate(`/${route}`);
-                }
-              }}
-            >
-              {label}
-            </li>
-          ))}
+          {navElements.map(({ label, route, style = {} }) => {
+            if (route === "cart") {
+              return (
+                <li
+                  onClick={() => {
+                    navigate(`/${route}`);
+                  }}
+                  key={route}
+                  style={style}
+                >
+                  {label}
+                </li>
+              );
+            }
+            return (
+              <li
+                style={style}
+                key={label}
+                onClick={() => {
+                  if (label === "Menu") {
+                    setShowSidebar(true);
+                  } else {
+                    navigate(`/${route}`);
+                  }
+                }}
+              >
+                {label}
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </>
