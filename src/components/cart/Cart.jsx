5332228d9d15c8158f8b37ed19e83bfa72dd2989
Josh Burd <Jburdt@gmail.com>
2023-11-19 21:19:17 -0500

    updated cart

---
+++ src/components/cart/Cart.jsx
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux";
+
+const Cart = () => {
+  const cart = useSelector((store) => store.cartItems);
+
+  if (cart.length === 0) {
+    return <div>THere are no items in the cart.</div>
+  }
+  return(
+    <div>
+      <h2>Cart</h2>
+      <ul>
+        {cart.map((item, idx) => (
+          <li key={`${item.id}- ${idx}`}>{item.name}</li>
+        ))}
+      </ul>
+      <button>Buy Everything</button>
+    </div>
+  ) 
+};
+
+export default Cart;
